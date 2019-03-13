df2e1692c4513dc281f09c1bf39e0f0bcbb550f4
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-03-13 15:13:46 +0800

    Valid Parentheses

---
+++ src/com/leetcode/algorithms/ValidParentheses.java
@@ -0,0 +1,45 @@
+package com.leetcode.algorithms;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ValidParentheses {
+	
+	public static void main(String...strings) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(isValid(scanner.next()));
+		
+		scanner.close();
+	}
+	
+	public static boolean isValid(String s) {
+		// Check if s length is even
+        if(s.length() % 2 != 0) {
+        	return false;
+        }
+		
+        Stack<Character> stack = new Stack<>();
+		for (Character bracket : s.toCharArray()) {
+			switch (bracket) {
+				case '{':
+					stack.push('}');
+					break;
+				case '(':
+					stack.push(')');
+					break;
+				case '[':
+					stack.push(']');
+					break;
+				default:
+					if(stack.empty() || bracket != stack.peek()) { // Check if stack is empty or top of stack is close bracket
+						return false;
+					}
+				stack.pop();
+			}
+		}
+		
+		return stack.isEmpty();
+    }
+
+}
