67e2897b68f29dee51dc905746245a8d1e71c22f
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-03-15 03:57:46 +0800

    Available captures for Rook (2nd attempt)

--- src/com/leetcode/algorithms/AvailableCapturesForRook.java
+++ src/com/leetcode/algorithms/AvailableCapturesForRook.java
@@ -1,12 +1,9 @@
 package com.leetcode.algorithms;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
-// LATER
 public class AvailableCapturesForRook {
 	
 	public static void main(String...strings) {
@@ -28,55 +25,78 @@ public class AvailableCapturesForRook {
 	}
 	
 	public static int numRookCaptures(char[][] board) {
+		String xAxisRook = ""; // j
+        String yAxisRook = ""; // i
+		
 		// location, chess pieces
 		Map<String, String> chessPieces = new HashMap<>();
-		Map<String, String> rook = new HashMap<>();
+		
         for(int i = 0; i < board.length; i++) {
         	for(int j = 0; j < board[i].length; j++) {
         		String key = Integer.toString(j) + Integer.toString(i);
         		String value = Character.toString(board[i][j]);
         		
         		if(!".".equals(value)) {
-        			chessPieces.put(key, value);
-        			
         			if("R".equals(value)) {
-            			rook.put(key, value);
+            			xAxisRook = Integer.toString(j);
+                    	yAxisRook = Integer.toString(i);
+            		} else {
+            			chessPieces.put(key, value);
             		}
         		}
         	}
         }
         
-        String xAxisRook = ""; // j
-        String yAxisRook = ""; // i
-        for(Map.Entry<String, String> r : rook.entrySet()) {
-        	String coordinate = r.getKey();
-        	xAxisRook = coordinate.substring(0, 1);
-        	yAxisRook = coordinate.substring(1, 2);
-        }
+        int result = 0;
         
-        Set<String> count = new HashSet<>();
+        int diff = 0;
         
         for(Map.Entry<String, String> chessPiece : chessPieces.entrySet()) {
         	String coordinate = chessPiece.getKey();
         	String x = coordinate.substring(0, 1);
         	String y = coordinate.substring(1, 2);
         	String piece = chessPiece.getValue();
-        	if("p".equals(piece)) {
-        		if(xAxisRook.equals(x) || yAxisRook.equals(y)) {
-        			if(xAxisRook.equals(x) && (Integer.parseInt(y) > Integer.parseInt(yAxisRook))) {
-        				count.add("+Y");
-        			} else if(xAxisRook.equals(x) && (Integer.parseInt(y) < Integer.parseInt(yAxisRook))) {
-        				count.add("-Y");
-        			} else if(yAxisRook.equals(y) && (Integer.parseInt(x) > Integer.parseInt(xAxisRook))) {
-        				count.add("+X");
-        			} else if(yAxisRook.equals(y) && (Integer.parseInt(x) < Integer.parseInt(xAxisRook))) {
-        				count.add("-X");
-        			}
-        		}
+        	
+        	int currentDiff = 0;
+        	
+        	if(xAxisRook.equals(x)) {
+        		if(diff == 0) {
+					diff = Math.abs(Integer.parseInt(y) - Integer.parseInt(yAxisRook));
+				}
+        		currentDiff = Math.abs(Integer.parseInt(y) - Integer.parseInt(yAxisRook));
         	}
+        	
+        	if(yAxisRook.equals(y)) {
+        		if(diff == 0) {
+					diff = Math.abs(Integer.parseInt(x) - Integer.parseInt(xAxisRook));
+				}
+        		currentDiff = Math.abs(Integer.parseInt(x) - Integer.parseInt(xAxisRook));
+        	}
+        	
+        	if(xAxisRook.equals(x) && (Integer.parseInt(y) > Integer.parseInt(yAxisRook)) && (currentDiff <= diff)) {
+				if("p".equals(piece)) {
+					++result;
+				}
+				diff = currentDiff;
+			} else if(xAxisRook.equals(x) && (Integer.parseInt(y) < Integer.parseInt(yAxisRook)) && (currentDiff <= diff)) {
+				if("p".equals(piece)) {
+					++result;
+				}
+				diff = currentDiff;
+			} else if(yAxisRook.equals(y) && (Integer.parseInt(x) > Integer.parseInt(xAxisRook)) && (currentDiff <= diff)) {
+				if("p".equals(piece)) {
+					++result;
+				}
+				diff = currentDiff;
+			} else if(yAxisRook.equals(y) && (Integer.parseInt(x) < Integer.parseInt(xAxisRook)) && (currentDiff <= diff)) {
+				if("p".equals(piece)) {
+					++result;
+				}
+				diff = currentDiff;
+			}
         }
 		
-		return count.size();
+		return result;
     }
 
 }
