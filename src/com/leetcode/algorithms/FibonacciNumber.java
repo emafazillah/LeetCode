664dda5a97fe6039610e32dc9fc4566fa5789f77
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-04-24 07:11:53 +0800

    Fibonacci Number (My submission)

--- src/com/leetcode/algorithms/FibonacciNumber.java
+++ src/com/leetcode/algorithms/FibonacciNumber.java
@@ -15,22 +15,20 @@ public class FibonacciNumber {
 	}
 	
 	public static int fib(int N) {
+		// Fibonacci, (FN) = (FN-1) + (FN-2)
 		BigInteger fn = new BigInteger("0");
+		BigInteger fnminus1 = new BigInteger("0");
+		BigInteger fnminus2 = new BigInteger("1");
 		
 		for(int i = 0; i < N; i++) {
-			BigInteger input = new BigInteger(Integer.toString(N));
-			// Fibonacci, (FN) = (FN-1) + (FN-2)
-			// BigInteger fn = new BigInteger("0");
-			BigInteger fnminus1 = new BigInteger("0");
-			BigInteger fnminus2 = new BigInteger("1");
-			//long count = 0L;			
-			while(fn.compareTo(input) != 1) {
-				fn = fnminus1.add(fnminus2);
-				fnminus2 = fnminus1;
-				fnminus1 = fn;
-				//count++;
-			}
-			//result += Long.toString(count - 1) + " ";
+			//System.out.println("fnminus1: " + fnminus1);
+			//System.out.println("fnminus2: " + fnminus2);
+			
+			fn = fnminus1.add(fnminus2);
+			fnminus2 = fnminus1;
+			fnminus1 = fn;
+			
+			//System.out.println("fn: " + fn);
 		}
 		
 		return fn.intValue();
