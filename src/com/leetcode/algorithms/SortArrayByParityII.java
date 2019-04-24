37bea2bbe1a28a1ed750570c36be7c4764a742c2
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-04-24 10:10:01 +0800

    Sort Array By Parity II (My submission)

--- src/com/leetcode/algorithms/SortArrayByParityII.java
+++ src/com/leetcode/algorithms/SortArrayByParityII.java
@@ -1,24 +1,45 @@
 package com.leetcode.algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class SortArrayByParityII {
 	
 	public static void main(String...strings) {
+		// Input
 		Scanner scanner = new Scanner(System.in);
+		List<String> inputs = new ArrayList<>();
+		while(scanner.hasNext()) {
+			String input = scanner.next();
+			if("exit".equals(input)) {
+				break;
+			}
+			
+			inputs.add(input);
+		}
 		
+		// Convert Collection into Array
+		int[] A = new int[inputs.size()];
+		for(int i = 0; i < A.length; i++) {
+			A[i] = Integer.parseInt(inputs.get(i));
+		}
+		
+		// Print output
+		System.out.println(Arrays.toString(sortArrayByParityII(A)));
 		
 		scanner.close();
 	}
 	
-	public int[] sortArrayByParityII(int[] A) {
+	public static int[] sortArrayByParityII(int[] A) {
         int[] result = new int[A.length];
 		
-        int countEven = 0;
+        
         int[] even = new int[A.length / 2];
-        int countOdd = 0;
         int[] odd = new int[A.length / 2];
-        String firstMap = "Even";
+        int countEven = 0;
+        int countOdd = 0;
         
         // Sort odd or even
         for(int i = 0; i < A.length; i++) {
@@ -27,20 +48,22 @@ public class SortArrayByParityII {
         		even[countEven] = item;
         		++countEven;
         	} else {
-        		if(i == 0) {
-        			firstMap = "Odd";
-        		}
-        		
         		odd[countOdd] = item;
         		++countOdd;
         	}
         }
         
         // Sort by parity
+        countEven = 0;
+        countOdd = 0;
         for(int i = 0; i < result.length; i++) {
-        	if("Even".equals(firstMap)) {
-        		
-        	}
+        	if(i % 2 == 0) {
+    			result[i] = even[countEven];
+    			++countEven;
+    		} else {
+    			result[i] = odd[countOdd];
+    			++countOdd;
+    		}
         }
 		
         return result;
