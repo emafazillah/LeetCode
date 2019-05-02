d9f2fbbc66ec54955b4fb036533da661b1afb570
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-05-02 19:24:45 +0800

    Search in a Binary Search Tree (Not optimised)

--- src/com/leetcode/algorithms/easy/SearchInABinarySearchTree.java
+++ src/com/leetcode/algorithms/easy/SearchInABinarySearchTree.java
@@ -1,10 +1,17 @@
 package com.leetcode.algorithms.easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class SearchInABinarySearchTree {
 	
+	private static List<Integer> outputTreeNodeList;
+	
 	public static void main(String...strings) {
+		outputTreeNodeList = new ArrayList<>();
+		
 		// Input
 		Scanner scanner = new Scanner(System.in);
 		String inputTree = scanner.next();
@@ -23,6 +30,8 @@ public class SearchInABinarySearchTree {
 		
 		// Print output
 		searchBST(inputTreeNode, inputSearch);
+		Integer[] outputTreeNodeArray = new Integer[outputTreeNodeList.size()];
+		System.out.println(Arrays.toString(outputTreeNodeList.toArray(outputTreeNodeArray)));
 		
 		scanner.close();
 	}
@@ -34,9 +43,9 @@ public class SearchInABinarySearchTree {
 		}
 		
 		if(input > treeNode.val) {
-			treeNode.right = insertTreeNode(treeNode, input);
+			treeNode.right = insertTreeNode(treeNode.right, input);
 		} else if(input < treeNode.val) {
-			treeNode.left = insertTreeNode(treeNode, input);
+			treeNode.left = insertTreeNode(treeNode.left, input);
 		}
 		
 		return treeNode;
@@ -55,6 +64,19 @@ public class SearchInABinarySearchTree {
         // Search left
         else if(val < root.val) {
         	root.left = searchBST(root.left, val);
+        }
+        
+        // Insert into array
+        else {
+        	outputTreeNodeList.add(root.val);
+        	
+        	if(root.left != null) {
+        		outputTreeNodeList.add(root.left.val);
+        	}
+        	
+        	if(root.right != null) {
+        		outputTreeNodeList.add(root.right.val);
+        	}
         }
 		
 		return root;
