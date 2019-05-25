f9db992acc998d2de277356f1ad4d5da50301707
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-05-25 09:49:42 +0800

    Implement Queue using Stacks (2nd submission)

--- src/com/leetcode/algorithms/easy/ImplementQueueUsingStacks.java
+++ src/com/leetcode/algorithms/easy/ImplementQueueUsingStacks.java
@@ -17,6 +17,7 @@ public class ImplementQueueUsingStacks {
 		MyQueue obj = new MyQueue();
 		obj.push(1);
 		obj.push(2);
+		//obj.push(3);
 		System.out.println("pop: " + obj.pop());
 		System.out.println("peek: " + obj.peek());
 		System.out.println("is empty: " + obj.empty());
@@ -46,11 +47,26 @@ class MyQueue {
     		this.stack.push(x);
     	} else {
     		Stack<Integer> tempStack = new Stack<>();
-    		while(!this.stack.isEmpty()) {
-    			tempStack.push(this.stack.pop());
-    		}
+    		
+//    		while(!this.stack.isEmpty()) {
+//    			tempStack.push(this.stack.pop());
+//    		}
+//    		tempStack.push(x);
+//    		this.stack = tempStack;
+    		
     		tempStack.push(x);
-    		this.stack = tempStack;
+            int size = this.stack.size();
+            int count = 0;
+            while(count < size) {
+            	tempStack.push(this.stack.pop());
+            	++count;
+            }
+    		
+            // Update stack
+            while(size > -1) {
+            	this.stack.push(tempStack.pop());
+            	--size;
+            }
     	}
     }
     
@@ -80,7 +96,7 @@ class MyQueue {
         	throw new RuntimeException("Stack is empty");
         }
     	
-    	return stack.get(0);
+    	return this.stack.get(0);
     }
     
     /** Returns whether the queue is empty. */
