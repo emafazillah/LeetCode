f493a603df833e70b47d2f0a0e8b60a498b0879d
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-05-31 12:57:25 +0800

    Valid Perfect Square (3rd submission)

--- src/com/leetcode/algorithms/easy/binarysearch/ValidPerfectSquare.java
+++ src/com/leetcode/algorithms/easy/binarysearch/ValidPerfectSquare.java
@@ -12,13 +12,17 @@ public class ValidPerfectSquare {
 	}
 	
 	public static boolean isPerfectSquare(int num) {
+		if(num == 1) {
+			return true;
+		}
+		
 		int left = 1;
 		int right = num / 2;
 		while(left <= right) {
 			int mid = left + (right - left) / 2;
 			if (num == mid * mid) {
 				return true;
-			} else if(num > mid + mid) {
+			} else if(num > mid * mid) {
 				left = mid + 1;
             } else {
                 right = mid - 1;
