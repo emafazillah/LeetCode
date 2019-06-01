7aaa5f4b041f52dffd051cd194b8797a53e6c54b
Ema Fazillah binti Abu Bakar <gatsu2811@gmail.com>
2019-06-01 23:16:45 +0800

    Time Based Key-Value Store (Initial commit)

---
+++ src/com/leetcode/algorithms/medium/binarysearch/TimeMap.java
@@ -0,0 +1,29 @@
+package com.leetcode.algorithms.medium.binarysearch;
+
+public class TimeMap {
+	
+	/** Initialize your data structure here. */
+    public TimeMap() {
+        
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        
+    }
+    
+    public String get(String key, int timestamp) {
+    	return "";
+    }
+    
+    public static void main(String...strings) {
+    	String key = "";
+    	String value = "";
+    	int timestamp = 0;
+    	
+    	// Your TimeMap object will be instantiated and called as such:
+    	TimeMap obj = new TimeMap();
+    	obj.set(key, value, timestamp);
+    	String param_2 = obj.get(key, timestamp);
+    }
+    
+}
