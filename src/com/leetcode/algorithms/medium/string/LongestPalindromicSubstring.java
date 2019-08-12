be1094efea979d2329176cf638733a4905c74aa3
emafazillah <gatsu2811@gmail.com>
2019-08-12 17:16:31 +0800

    Longest Palindromic Substring time limit exceed

--- src/com/leetcode/algorithms/medium/string/LongestPalindromicSubstring.java
+++ src/com/leetcode/algorithms/medium/string/LongestPalindromicSubstring.java
@@ -15,47 +15,66 @@ public class LongestPalindromicSubstring {
 		scanner.close();
 	}
 
-	static String longestPalindrome(String input) {
-		String longestPalindromicString = "";
-		
-		for (int midPoint = 1; midPoint < input.length() - 1; midPoint++) {
-			for (int i = 1; i <= midPoint; i++) {
-				String left = "";
-				String right = "";				
-				
-				if ((midPoint - i > -1) && (midPoint + i < input.length())) {
-					if (input.charAt(midPoint) == input.charAt(midPoint + 1)) {
-						left = input.substring(midPoint - (i - 1), midPoint + 1);
-						right = input.substring(midPoint + 1, midPoint + 1 + i);
-					} else {
-						left = input.substring(midPoint - i, midPoint);
-						right = input.substring(midPoint + 1, midPoint + 1 + i);
-					}					
-				}
-				
-				boolean isPalindromic = true;
-				
-				for (int j = 0; j < left.length(); j++) {
-					if (left.charAt(j) != right.charAt(left.length() - 1 - j)) {
-						isPalindromic = false;
-						break;
-					}
-				}
-				
-				if (isPalindromic) {
-					String palindromicString = left + input.charAt(midPoint) + right;
-					if (input.charAt(midPoint) == input.charAt(midPoint + 1)) {
-						palindromicString = left + right;
+	static String longestPalindrome(String s) {
+		if (s.length() == 1) {
+			return s;
+		} else {
+			String longestPalindromicString = "";
+			
+			for (int midPoint = 0; midPoint < s.length(); midPoint++) {
+				for (int i = 0; i <= midPoint; i++) {
+					String oddLeft = "";
+					String oddRight = "";
+					String evenLeft = "";
+					String evenRight = "";
+					
+					if (midPoint - i >= 0 && midPoint + 1 + i <= s.length()) {
+						oddLeft = s.substring(midPoint - i, midPoint);
+						oddRight = s.substring(midPoint + 1, midPoint + 1 + i);
 					}
 					
-					if (palindromicString.length() > longestPalindromicString.length()) {
-						longestPalindromicString = palindromicString;
+					if (midPoint - i >= 0 && midPoint + 2 + i <= s.length()) {
+						evenLeft = s.substring(midPoint - i, midPoint + 1);
+						evenRight = s.substring(midPoint + 1, midPoint + 2 + i);
 					}
-				}				
+					
+					boolean isPalindromic = true;
+					
+					for (int j = 0; j < oddLeft.length(); j++) {
+						if (oddLeft.charAt(j) != oddRight.charAt(oddLeft.length() - 1 - j)) {
+							isPalindromic = false;
+							break;
+						}
+					}
+					
+					if (isPalindromic) {
+						String palindromicString = oddLeft + s.charAt(midPoint) + oddRight;
+						
+						if (palindromicString.length() > longestPalindromicString.length()) {
+							longestPalindromicString = palindromicString;
+						}
+					}
+					
+					isPalindromic = true;
+					
+					for (int j = 0; j < evenLeft.length(); j++) {
+						if (evenLeft.charAt(j) != evenRight.charAt(evenLeft.length() - 1 - j)) {
+							isPalindromic = false;
+							break;
+						}
+					}
+					
+					if (isPalindromic) {
+						String palindromicString = evenLeft + evenRight;
+						
+						if (palindromicString.length() > longestPalindromicString.length()) {
+							longestPalindromicString = palindromicString;
+						}
+					}
+				}
 			}
+			
+			return longestPalindromicString;			
 		}
-		
-		return longestPalindromicString;
 	}
-	
 }
