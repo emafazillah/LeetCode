a5e2a8360d42ebea9efb3ba0fbcfdbdbce3afb73
emafazillah <gatsu2811@gmail.com>
2019-09-24 01:16:36 +0800

    Find Mode In Binary Search Tree

--- src/com/leetcode/algorithms/easy/binarysearch/FindModeInBinarySearchTree.java
+++ src/com/leetcode/algorithms/easy/binarysearch/FindModeInBinarySearchTree.java
@@ -1,6 +1,8 @@
 package com.leetcode.algorithms.easy.binarysearch;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -22,7 +24,10 @@ public class FindModeInBinarySearchTree {
 		}
 		
 		// Find the duplicate
-		findMode(root);
+		int[] result = findMode(root);
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
 		
 		scanner.close();
 	}
@@ -41,20 +46,56 @@ public class FindModeInBinarySearchTree {
 		return root;
 	}
 	
-	static Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-	
-	static int[] findMode(TreeNode root) {
-		if(root == null) {
-			return null;
-		}
-		
-        if(map.isEmpty()) {
-        	map.put(root.val, 1);
+	static TreeNode searchTreeNode(TreeNode root, Map<Integer, Integer> map) {
+		if (map.containsKey(root.val)) {
+        	int val = map.get(root.val) + 1;
+        	map.put(root.val, val);
         } else {
-        	
+        	map.put(root.val, 1);
         }
         
-        // TODO
-        return null;
+        // Search left
+        if (root.left != null) {
+        	root.left = searchTreeNode(root.left, map);
+        }
+        
+        // Search right
+        if (root.right != null) {
+        	root.right = searchTreeNode(root.right, map);
+        }
+        
+        return root;
+	}
+	
+	static int[] findMode(TreeNode root) {
+		if (root == null) {
+			return new int[0];
+		}
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		searchTreeNode(root, map);
+        
+        // Get mode
+		int mode = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        	if (entry.getValue() >= mode) {
+        		mode = entry.getValue();
+        	}
+        }
+        
+        // Update list
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        	if (entry.getValue() == mode) {
+        		list.add(entry.getKey());
+        	}
+        }
+        
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+        	result[i] = list.get(i);
+        }
+        
+        return result;
     }
 }
