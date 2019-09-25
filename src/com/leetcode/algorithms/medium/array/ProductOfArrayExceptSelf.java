181f1ea66dfa2a3af2f920140a32762b8298038f
emafazillah <gatsu2811@gmail.com>
2019-09-26 02:45:50 +0800

    Product of Array Except Self (Final submission)

--- src/com/leetcode/algorithms/medium/array/ProductOfArrayExceptSelf.java
+++ src/com/leetcode/algorithms/medium/array/ProductOfArrayExceptSelf.java
@@ -24,15 +24,21 @@ public class ProductOfArrayExceptSelf {
         	return nums;
         }
         
-        int[] result = new int[nums.length];
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < left.length; i++) {
+        	left[i] = nums[i - 1] * left[i - 1];
+        }
         
+        int[] right = new int[nums.length];
+        right[right.length - 1] = 1;
+        for (int i = right.length - 2; i >= 0; i--) {
+        	right[i] = nums[i + 1] * right[i + 1];
+        }
+        
+        int[] result = new int[nums.length];
         for (int i = 0; i < result.length; i++) {
-        	result[i] = 1;
-        	for (int j = 0; j < result.length; j++) {
-        		if (i != j) {
-        			result[i] = result[i] * nums[j];
-        		}
-        	}
+        	result[i] = left[i] * right[i];
         }
         
         return result;
